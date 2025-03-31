ffcb2ebfac4a5d011555c40c713da3ee762c4543
nikita-sychev49ru <hesoyame1998@gmail.com>
2025-03-31 06:09:33 +0300

    add Readme, hwrk10.1 done

--- src/processing.py
+++ src/processing.py
@@ -13,9 +13,3 @@ def sort_by_date(operations: list[dict], sort: bool = True) -> list[dict]:
 
     sorted_operations = sorted(operations, key=lambda item: item["date"], reverse=sort)
     return sorted_operations
-
-
-test_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
